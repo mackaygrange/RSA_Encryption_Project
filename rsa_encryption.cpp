3c3cfc77a719070f8085174261c3ef1e4ef2882f
Mackay Grange <mackay.grange@gmail.com>
2024-02-15 13:54:09 -0700

    laptop updates.

--- rsa_encryption.cpp
+++ rsa_encryption.cpp
@@ -5,22 +5,24 @@
 #include <numeric>
 #include <cmath>
 
-bool prime_check(int n)
+int validate_prime(int n)
 {
     if (n <= 1)
     {
-        return false;
+        std::cout << "Error, the integer must be prime. " << n << " is not a prime number." << std::endl;
+        return 0;
     }
 
     for (unsigned int i = 2; i <= (n / 2); i++)
     {
         if (n % i == 0)
         {
-            return false;
+            std::cout << "Error, the integer must be prime. " << n << " is not a prime number." << std::endl;
+            return 0;
         }
     }
 
-    return true;
+    return n;
 }
 
 int find_gcd(int e, int lambda)
@@ -30,28 +32,31 @@ int find_gcd(int e, int lambda)
     return find_gcd(lambda, e % lambda);   
 }
 
-int find_e(int z)
+int validate_e(int e, int lambda)
 {
-    int e;
-    while (true)
+    if (e < 3 || e >= lambda)
     {
-        e = (rand() % z) + 1;
-
-        if (find_gcd(e, z) == 1)
-        {
-            return e;
-        }
+        std::cout << "Invalid lambda value. Value e must be between 3 and lambda - 1." << std::endl;
+        return 0;
     }
-    return 0;
+    else if (find_gcd(e, lambda != 1))
+    {
+        std::cout << "Invalid lambda value. Values e and lambda must be coprime." << std::endl;
+        return 0;
+    }
+    else
+    {
+        return e;
+    }
 }
 
-int find_d(int e, int z)
+int find_d(int e, int lambda)
 {
     int d;
-    for (unsigned int i = 1; i < z; i++)
+    for (unsigned int i = 1; i < lambda; i++)
     {
         d = i * e;
-        if (d % z == 1)
+        if (d % lambda == 1)
         {
             if (i != e)
             {
@@ -67,18 +72,70 @@ std::vector<int> private_key;
 
 int main()
 {
-    int p;
-    int q;
+    int p = 0;
+    int q = 0;
+    int t = 0;
 
-    std::cout << "Enter Prime Value \'p\':" << std::endl;
-    std::cin >> p;
-    std::cout << "Enter Prime Value \'q\':" << std::endl;
-    std::cin >> q;
+    while (!p)
+    {
+        std::cout << "Enter Prime Value \'p\': " << std::endl;
+        std::cin >> t;
+        p = validate_prime(t);
+        std::cout << std::endl;
+    }
+
+    while (!q)
+    {
+        std::cout << "Enter Prime Value \'q\': " << std::endl;
+        std::cin >> t;
+        q = validate_prime(t);
+        std::cout << std::endl;
+    }
 
     int n = p * q;
-    int lambda = (p - 1) * (q - 1); 
-    int e;
-    int d;
+    int lambda = (p - 1) * (q - 1);
+
+    std::cout << "Value of \'n\': " << n << std::endl;
+    std::cout << "Value of \'lambda(n)\': " << lambda << std::endl;
+    std::cout << std::endl;
+
+    int e = 0;
+
+    while (!e)
+    {
+        std::cout << "Enter Value \'e\': " << std::endl;
+        std::cin >> t;
+        e = validate_e(t, lambda);
+        std::cout << std::endl;
+    }
+    
+    int d = find_d(e, lambda);
+    std::cout << "Value of \'d\': " << d << std::endl;
+    std::cout << std::endl;
+
+    public_key.push_back(n);
+    private_key.push_back(n);
+    public_key.push_back(e);
+    private_key.push_back(d);
+
+    std::cout << "Public Key: {" << public_key.at(0) << ", " << public_key.at(1) << "}" << std::endl;
+    std::cout << "Private Key: {" << private_key.at(0) << ", " << private_key.at(1) << "}" << std::endl;
+    std::cout << std::endl;
+
+    int m;
+    std::cout << "Enter plaintext integer value \'m\': " << std::endl;
+    std::cin >> m;
+    std::cout << std::endl;
+
+    long double c = (long double) std::pow(m, public_key.at(1));
+    c = (unsigned long long int) c % public_key.at(0);
+    std::cout << "Encrypted Value of \'m\' (c): " << c << std::endl;
+    std::cout << std::endl;
+
+    long double m2 = (long double) std::pow(c, private_key.at(1));
+    m2 = (unsigned long long int) m2 % private_key.at(0);
+    std::cout << "Unencrypted Value of \'m\' (m2): " << m2 << std::endl;
+    std::cout << std::endl;
 
     return 0;
 }
